54cdc0202dded931f0d6e7b45234fd275fc61399
Maria Vasilenko <snysic1987@gmail.com>
2024-11-15 12:47:26 +0100

    change BadToy

--- src/main/java/models/BadToy.java
+++ src/main/java/models/BadToy.java
@@ -1,5 +1,19 @@
 package models;
 
-public class BadToy {
+public class BadToy extends Toy {
+    private String content;
 
+    public BadToy(String title, String content) {
+        super(title, false);
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Título: %s, Contenido: %s", title, content);
+    }
 }
