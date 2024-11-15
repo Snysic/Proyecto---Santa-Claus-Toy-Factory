38d69694223085ae3bcca859ea5dc4a5ac3c5adb
Maria Vasilenko <snysic1987@gmail.com>
2024-11-15 12:44:15 +0100

    change GoodToy

--- src/main/java/models/GoodToy.java
+++ src/main/java/models/GoodToy.java
@@ -1,13 +1,12 @@
 package models;
 
 public class GoodToy extends Toy {
-
     private String brand;
     private int targetAge;
     private String category;
 
-    public GoodToy(String title, String brand, int targetAge, String category, boolean isGoodToy) {
-        super(title, isGoodToy);
+    public GoodToy(String title, String brand, int targetAge, String category) {
+        super(title, true);
         this.brand = brand;
         this.targetAge = targetAge;
         this.category = category;
@@ -17,24 +16,17 @@ public class GoodToy extends Toy {
         return brand;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
     public int getTargetAge() {
         return targetAge;
-    }
-
-    public void setTargetAge(int targetAge) {
-        this.targetAge = targetAge;
     }
 
     public String getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    @Override
+    public String toString() {
+        return String.format("Título: %s, Marca: %s, Edad Recomendada: %d, Categoría: %s",
+                title, brand, targetAge, category);
     }
-
 }
